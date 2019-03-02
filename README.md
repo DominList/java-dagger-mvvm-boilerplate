74338a4e26f3ca7ddcdbeadacb274a883e7d9eb2
Mykyta Sikriier <sikrinick@gmail.com>
2019-03-02 16:13:31 +0100

    added default dependencies

---
+++ README.md
@@ -0,0 +1,5 @@
+Boilerplate for Android app in Java
+Tech stack:
+Dagger 2
+RxJava
+ViewModel + LiveData
---
+++ app/src/main/java/com/sikrinick/java_dagger_boilerplate/BoilerplateApplication.java
@@ -0,0 +1,11 @@
+package com.sikrinick.java_dagger_boilerplate;
+
+import android.app.Application;
+
+public class BoilerplateApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
